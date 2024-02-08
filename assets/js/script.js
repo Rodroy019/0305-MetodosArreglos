97ef06c8f0c6003f01bf03d737db499ad05d6c44
Rodrigo Rivas <rodroy019@hotmail.com>
2024-02-08 19:58:22 -0300

    Add: se añade funciones de agregar con validacion, check y borrar

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,63 +2,70 @@ const btnAgregar = document.getElementById("btnAgregar");
 const inputTarea = document.getElementById("inputTarea");
 const spanTotal = document.getElementById("spanTotal"); 
 const spanRealizadas = document.getElementById("spanRealizadas"); 
-const idList = document.getElementById("idList");
-const tareaList = document.getElementById("tareaList");
-const checkList = document.getElementById("checkList");
-const borrarList = document.getElementById("borrarList");
 const divList = document.getElementById("divList");
 
-
-
+let contadorID = 4;
 let tareas = [
     { id: 1, descripcion: "Cambiar Pañales", realizada: true },
     { id: 2, descripcion: "Preparar Mamdera", realizada: false },
     { id: 3, descripcion: "Hacer comida para el Bebé", realizada: false }
 ];
 
+    let agregar = () => {
+        if(inputTarea.value != ''){
+            tareas.push({
+                id: contadorID, descripcion: inputTarea.value , realizada: false
+            });
+            contadorID++
+            inputTarea.value = "";
+            list();
+        }else{
+            alert("Debes describir una tarea");
+            inputTarea.value = "";
+        }
+    };
 
-let agregar = () => {
-    tareas.push({
-        id: tareas.length +1, descripcion: inputTarea.value , realizada: false
-    })
-    console.log(tareas)
-    list()
-};
-
-let list = () => {
-    let templateId = "", templateTarea = "",templateCheck = "", templateBorrar ="", templateTotal="", TemplateRealizadas="";
-    for(let tarea of tareas){
-        templateId += `<li>${tarea.id}</li>`
-        templateTarea += `<li>${tarea.descripcion}</li>`
-        templateCheck += `<input type="checkbox"><br>`
-        templateBorrar += `<button class="btnBorrar">x</button><br>`
+    let list = () => {
+        let tablaHTML = '<table>';
+        tablaHTML += `
+            <tr>
+                <th>ID</th>
+                <th>Tareas</th>
+            </tr>`;
+        tareas.forEach(tarea => {
+        tablaHTML += `
+            <tr>
+                <td>${tarea.id}</td>
+                <td>${tarea.descripcion}</td>
+                <td><input type="checkbox" ${tarea.realizada ? 'checked' : ''} data-id="${tarea.id}" onchange="actualizarCheck(event)"></td>
+                <td><button onclick="borrar(${tarea.id})">x</button></td>
+            </tr>`;
+        });
+        tablaHTML += '</table>';
+        spanTotal.innerHTML = tareas.length;
+        checkMarcados();
+        divList.innerHTML = tablaHTML;
     }
-/*     idList.innerHTML = templateId;
-    tareaList.innerHTML = templateTarea;
-    checkList.innerHTML = templateCheck;
-    borrarList.innerHTML = templateBorrar;*/
-    spanTotal.innerHTML = tareas.length; 
-    divList.innerHTML = `
-    <ul id="idList">
-        <h4>ID</h4>`+
-        templateId
-    +`</ul>
-    <ul id="tareaList">
-        <h4>Tarea</h4>`+
-       templateTarea
-    +`</ul>
-    <ul id="checkList">
-        <br>`+
-       templateCheck
-    +`</ul>
-    <ul id="borrarList">
-        <br>`+
-        templateBorrar
-    +`</ul>
-    `
 
-}
+    function borrar(id) {
+        tareas.splice(tareas.findIndex(tarea => tarea.id === id), 1);
+        list();
+    }
 
+    function actualizarCheck(e) {
+        const id = parseInt(e.target.dataset.id);
+        const tarea = tareas.find(tarea => tarea.id === id);
+        tarea.realizada = e.target.checked;
+        checkMarcados();
+    }
+
+    function checkMarcados() {
+        let contador = 0;
+        tareas.forEach(tarea => {
+                if (tarea.realizada) {contador++;}
+            });
+        return spanRealizadas.innerHTML = contador;
+    }
 
 btnAgregar.addEventListener("click", agregar);
 
